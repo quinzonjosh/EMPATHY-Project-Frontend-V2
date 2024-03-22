574c6bab32871cb0fc565f483ed3b826a4d08c4f
kyla-dc <83800426+kyla-dc@users.noreply.github.com>
2024-03-22 20:49:42 +0800

    css edits

--- src/components/listeningTo/ListeningTo.jsx
+++ src/components/listeningTo/ListeningTo.jsx
@@ -3,28 +3,21 @@ import "./listeningTo.css";
 
 const ListeningTo = (props) => {
   return (
-    <div className="listening-to-label">
-      <div className="lt-label">We see that you're currently listening to...</div>
-      <div className="track-container">
-      <img className="track-image" src={props.url} />
-      <div className="track-info-container">
-        <div className="song-title">{props.title}</div>
-        <div className="artist-label">{props.artists.join(" | ")}</div>
-        <button className="refresh_button" onClick={() => props.fetchData()}>
-          Refresh
-          </button>
-      </div>
-
-      
-
-        <div className="info-button-container"> 
-          
-          
-
+  <div className="listening-to-label">
+    <div className="lt-label">We see that you're currently listening to...</div>
+    <div className="track-container">
+      <div className="pic-info-container">
+        <img className="track-image" src={props.url} />
+        <div className="track-info-container">
+          <div className="song-title">{props.title}</div>
+          <div className="artist-label">{props.artists.join(" | ")}</div>
         </div>
-
       </div>
+      <button className="refresh_button" onClick={() => props.fetchData()}>
+      Refresh
+      </button>
     </div>
+  </div>
   );
 };
 
